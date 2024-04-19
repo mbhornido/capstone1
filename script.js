0c320f68604f1292581fc3f8cb7cda55d6ae23cf
mbhornido <angelohornido2@gmail.com>
2024-04-19 10:48:18 +0800

    responsive header

---
+++ script.js
@@ -0,0 +1,44 @@
+function copyMenu(){
+    // copy inside .dpt-cat to .departments
+    var dptCategory = document.querySelector('.dpt-cat');
+    var dptPlace = document.querySelector('.departments');
+    dptPlace.innerHTML = dptCategory.innerHTML;
+
+    // copy inside nav to nav
+    var mainNav = document.querySelector('.header-nav nav');
+    var navPlace = document.querySelector('.off-canvas nav');
+    navPlace.innerHTML = mainNav.innerHTML;
+
+    // adfa
+    var topNav = document.querySelector('.header-top .wrapper');
+    var topPlace =document.querySelector('.off-canvas .thetop-nav');
+    topPlace.innerHTML = topNav.innerHTML;
+
+}
+
+copyMenu();
+
+//show mobile menu
+const menuButton = document.querySelector('.trigger'),
+      closeButton = document.querySelector('.t-close'),
+      addclass = document.querySelector('.site');
+menuButton.addEventListener('click', function(){
+    addclass.classList.toggle('showmenu')
+})
+closeButton.addEventListener('click',function() {
+    addclass.classList.remove('showmenu')
+})
+
+
+
+//show sub orgs in menu
+
+const submenu = document.querySelectorAll('.has-child .icon-small');
+submenu.forEach((menu) => menu.addEventListener('click', toggle));
+
+function toggle(e) {
+    e.preventDefault();
+    submenu.forEach((item) => item != this ? item.closest ('.has-child').classList.remove('expand') : null);
+    if (this.closest('.has-child').classList != 'expand');
+    this.closest('.has-child').classList.toggle('expand')
+}
